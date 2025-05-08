859c7265d7ecc0c35fbf854f889f2b0e4373a6cf
Nitesh292003 <nitesh.singh@joshsoftware.com>
2025-05-08 16:41:45 +0530

    test cases

--- Gemfile
+++ Gemfile
@@ -16,10 +16,10 @@ gem "turbo-rails"
 gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
-gem 'jwt' # For JWT authentication
-gem 'bcrypt' # For password hashing
-gem 'cancancan'
-gem 'rack-cors'
+gem "jwt" # For JWT authentication
+gem "bcrypt" # For password hashing
+gem "cancancan"
+gem "rack-cors"
 
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
@@ -48,22 +48,24 @@ gem "thruster", require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
-
+  
   # Static analysis for security vulnerabilities [https://brakemanscanner.org/]
   gem "brakeman", require: false
-
+  
   # Omakase Ruby styling [https://github.com/rails/rubocop-rails-omakase/]
   gem "rubocop-rails-omakase", require: false
+  gem "rspec-rails"
 end
 
 group :development do
-  # Use console on exceptions pages [https://github.com/rails/web-console]
-  gem "web-console"
-  gem "debug"
+  gem "rubocop", require: false
+  gem "rubocop-rails", require: false
 end
 
 group :test do
-  # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
-  gem "capybara"
-  gem "selenium-webdriver"
+  gem "factory_bot_rails"
+  gem 'faker'
+  gem 'shoulda-matchers'
 end
+
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,54 +1,55 @@
 class User < ApplicationRecord
-    include JwtConcern
-    has_secure_password
-  
-    has_many :event_bookings
-   # has_many :venues
-    has_many :event_types
-    has_many :events
-    has_many :discount_codes
-    
-  
-    # Associations
-    
-    belongs_to :role
-    has_many :events
-    has_many :discount_code_transactions
-  
-    validates :first_name, 
-              :surname, 
-              :phone_no, 
-              :password, 
-              :email, 
-              presence: { message: " fields can't be blank" }
-  
-    validates :email, 
-              format: { 
-                with: URI::MailTo::EMAIL_REGEXP, 
-                message: "must be a valid email address" 
-              }, 
-              uniqueness: { message: "is already in use by another user" }
-  
-    validates :phone_no, 
-              format: { 
-                with: /\A[0-9]{10}\z/, 
-                message: "must be a valid 10-digit phone number" 
-              }, 
-              uniqueness: { message: "is already in use by another user" }
-  
-    
+  include JwtConcern
+  has_secure_password
 
-              def is_admin?
-                role_id == 1
-              end
+  has_many :event_bookings
+  # has_many :venues
+  has_many :event_types
+  has_many :events
+  has_many :discount_codes
 
-              def is_user?
-                role_id == 2
-              end
 
-              def is_organizer?
-                role_id == 3
-              end
+  # Associations
 
-              
-  end
+  belongs_to :role
+  has_many :events
+  has_many :discount_code_transactions
+
+  validates :first_name,
+            :surname,
+            :phone_no,
+            :password,
+            :email,
+            presence: { message: "fields can't be blank" }
+
+  validates :email,
+            format: {
+              with: URI::MailTo::EMAIL_REGEXP,
+              message: "must be a valid email address"
+            },
+            uniqueness: { message: "is already in use by another user" }
+
+  validates :phone_no,
+            format: {
+              with: /\A[0-9]{10}\z/,
+              message: "must be a valid 10-digit phone number without spaces or special characters"
+            },
+            uniqueness: { message: "is already in use by another user" }
+  validates :password,
+            length: { minimum: 6, message: "must be at least 6 characters long" }
+
+
+
+
+            def is_admin?
+              role_id == 1
+            end
+
+            def is_user?
+              role_id == 2
+            end
+
+            def is_organizer?
+              role_id == 3
+            end
+end
